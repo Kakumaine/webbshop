b24dc1ec42e22f72f910203e07d0be10de55f9b7
Kakumaine <43368829+Kakumaine@users.noreply.github.com>
2018-09-18 16:01:11 +0200

    change css, js and html

--- produkter.js
+++ produkter.js
@@ -8,26 +8,34 @@ fetch("produkter.json")
 
 .then(function(data){
     prodData=data
-    console.log(prodData)
     createList()
 });
 
 function createList(){
     var productList=document.createElement("div")
     productList.className="productListClass"
-    console.log(productList)
 
     for(var index=0;index<prodData.length;index++) {
-        var prodName=document.createElement("h3")
-        var prodName=document.createTextNode(prodData[index].prodName)
-        var prodPrice=document.createTextNode(prodData[index].prodPrice)
+        var prodName=document.createElement("h4");
+        var getProdName=document.createTextNode(prodData[index].prodName)
+        prodName.appendChild(getProdName);
+        var prodPrice=document.createElement("h5");
+        var getProdPrice=document.createTextNode(prodData[index].prodPrice)
+        prodPrice.appendChild(getProdPrice)
         var prodImage=document.createElement("img")
         prodImage.src="bild/"+prodData[index].prodImage
+        prodImage.className="prodImageClass"
+
+
+
+        console.log()
+        console.log(prodName)
+        console.log(prodPrice)
+
         productList.appendChild(prodImage)
         productList.appendChild(prodName)
         productList.appendChild(prodPrice)
         document.body.appendChild(productList)
-        console.log()
     }
 }
 
